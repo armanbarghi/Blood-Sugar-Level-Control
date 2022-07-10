99a06948f77d7a0a3a9cefc82213727f1fbaa24c
Arman Barghi <arman.barghi@gmail.com>
2022-07-10 15:40:00 +0430

    Fix Linearize

--- main.m
+++ main.m
@@ -6,38 +6,37 @@ P2 = 0.025;
 P3 = 0.000013;
 V = 12;
 n = 5.54/60;
-syms G_dot X_dot I_dot G X I D U
+syms G X I U D
 F = [ -P1*G-X*(G+Gb)+D;
       -P2*X+P3*I;
       -n*(I+Ib)+U/V ];
 y = G;
 
+St = [G;X;I];
+In = [U;D];
+
 %% Equilibrium State
-% states = solve(subs(F,{D},{0})==[0;0;0], [G,X,I]);
-% Ge = states.G; Ie = states.I; Xe = states.X;
-Ge = Gb; Xe = 0; Ie = 0; Ue = n*Ib*V;
-Fe = subs(F,{G,X,I,U},{Ge,Xe,Ie,Ue});
+% sol_s = solve(subs(F,{D},{0})==[0;0;0], [G,X,I]);
+% Ge = sol_s.G; Ie = sol_s.I; Xe = sol_s.X;
+St_e = [0;0;0];
+In_e = [n*Ib*V;0];
+Fe = subs(subs(F,In,In_e),St,St_e);
 
 %% Linearize
-% f(x) = f(x0) + transpose(grad(f,x)|x=x0)*(x-x0)
-grad_F = [ diff(F(1),G), diff(F(1),X), diff(F(1),I);
-           diff(F(2),G), diff(F(2),X), diff(F(2),I);
-           diff(F(3),G), diff(F(3),X), diff(F(3),I) ];
-lin_F = Fe + subs(grad_F,{G,X,I,U},{Ge,Xe,Ie,Ue})*([G;X;I]-[Ge;Xe;Ie]);
-A = [ diff(lin_F(1),G), diff(lin_F(1),X), diff(lin_F(1),I);
-      diff(lin_F(2),G), diff(lin_F(2),X), diff(lin_F(2),I);
-      diff(lin_F(3),G), diff(lin_F(3),X), diff(lin_F(3),I) ];
-B = [ diff(lin_F(1),U) ; diff(lin_F(2),U) ; diff(lin_F(3),U) ];
-vpa(lin_F,3)
-vpa(A,3)
-vpa(B,3)
-D = 0;
-C = [1,0,0];
+% f(x) = f(xe,ue) + (grad(f,x)|x=xe,u=ue)*(x-xe) + (grad(f,u)|x=xe,u=ue)*(u-ue)
+grad_F_St = [ diff(F(1),St(1)), diff(F(1),St(2)), diff(F(1),St(3));
+              diff(F(2),St(1)), diff(F(2),St(2)), diff(F(2),St(3));
+              diff(F(3),St(1)), diff(F(3),St(2)), diff(F(3),St(3)) ];
+grad_F_In = [ diff(F,In(1)) , diff(F,In(2)) ];
+A = subs(subs(grad_F_St,In,In_e),St,St_e)
+B = subs(subs(grad_F_In,In,In_e),St,St_e)
+C = [1,0,0]
+D = 0
 
 %% Transfer Function
 syms s
 n = size(A,1);
-G = C*inv(s*eye(n)-A)*B
+G = C*inv(s*eye(n)-A)*B;
 ExpFun = matlabFunction(simplifyFraction(G,'Expand',true));
 ExpFun = str2func(regexprep(func2str(ExpFun), '\.([/^\\*])', '$1'));
 G = tf(ExpFun(tf('s')));
